0c47dda79f09f009fc40003a1b1e8e79ad95cb1a
Zarlanga <robertozitto@gmail.com>
2022-09-09 05:32:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* var bod = document.querySelector("body");
+bod += " ";
+
+for (let i = 1; i <= 8; i++) {
+ bod.innerText += `<div class="papa" id="grItB${i}"> ${i} </div> \n`;
+  
+  //`#grIt${i} { \n\tgrid-area: ga${i};\n }\n`
+  
+  //`<div class="pepe" id="grIt${i}"> ${i} </div> \n`
+}
+bod.innerText  += "\n\n"
+for (let i = 1; i <= 8; i++) {
+  bod.innerText  += `#grItB${i} { \n\tgrid-area: gb${i};\n }\n`
+} */
